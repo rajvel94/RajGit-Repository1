ca6b4784431ead067786ff2c9d4b11b9a8dc7473
rajvel94 <56280359+rajvel94@users.noreply.github.com>
2019-10-12 10:41:44 -0400

    Add files via upload

---
+++ _Capstone project notebook.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Name : Selvaraj Vealyudham
+# #This notebook will be mainly used for the capstone project
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[7]:
+
+
+import numpy as np
+
+
+# In[14]:
+
+
+print ('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
